df8d88bbb3161eb0795192459cc413d55d990bc2
vishalxk <github@vishalxk.com>
2023-12-28 14:12:00 +0530

    Conditionally render live-editable toast preview.

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Button from "../Button";
+import Toast from "../Toast";
 
 import styles from "./ToastPlayground.module.css";
 
@@ -9,6 +10,11 @@ const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  function toggleVisibility() {
+    setIsVisible(!isVisible);
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +22,12 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      {isVisible && (
+        <Toast variant={variant} toggleVisibility={toggleVisibility}>
+          {message}
+        </Toast>
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -65,7 +77,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={toggleVisibility}>Pop Toast!</Button>
           </div>
         </div>
       </div>
